3964c657375e30ec1e51b1e7562d3ae6ba0b1ab8
Daniel Frick <danielfrick7@gmail.com>
2017-09-20 23:22:27 -0500

    CP-22-Validations Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-resources :topics do
-  resources :posts, except: [:index]
+  resources :topics do
+    resources :posts, except: [:index]
 end
 
 get 'about' => 'welcome#about'
