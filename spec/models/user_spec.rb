6e837703215c63abeed54e59021c9da6f13c59c8
Daniel Frick <danielfrick7@gmail.com>
2017-09-24 09:45:25 -0500

    Assign-23-Auth-User Completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-  # Shoulda tests for name
+  # Should tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  # Shoulda tests for email
+  # Should tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
-  # Shoulda tests for password
+  # Should tests for password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }
@@ -22,7 +22,14 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format user name" do
+      user.name = "user name"
+      user.save
+      expect(user.name).to eq "User Name"
+    end
   end
+
   describe "invalid user" do
   let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
   let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
