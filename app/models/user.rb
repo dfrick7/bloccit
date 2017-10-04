edad517f0818056505c62b05179c24012d0fe3d3
Daniel Frick <danielfrick7@gmail.com>
2017-10-03 21:17:33 -0500

    CP-27-Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,20 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+
   before_save { self.email = email.downcase if email.present? }
+  before_save { self.role ||= :member }
 
-# #3
-validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
-validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
-validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
-validates :email,
-          presence: true,
-          uniqueness: { case_sensitive: false },
-          length: { minimum: 3, maximum: 254 }
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
-# #6
-has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, allow_blank: true
+
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 254 }
+
+  has_secure_password
+
+  enum role: [:member, :admin]
 end
